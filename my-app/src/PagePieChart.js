19611d2428563dd3fc0e9d2aa6c1c153fc544847
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-06-20 20:35:03 +0900

    style: datalabels 설정 (카테고리, 금액, 퍼센티지)

--- my-app/src/PagePieChart.js
+++ my-app/src/PagePieChart.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { categoryColors } from './constants';
 import useFetchData from './useFetchData';
 import './styles.css';
 
 Chart.register(...registerables);
+Chart.register(ChartDataLabels);
 
 const PieChartPage = () => {
   const { data, error } = useFetchData('/data.csv');
@@ -43,26 +45,70 @@ const PieChartPage = () => {
       datasets: [{
         data: categoryAmounts.map(item => item.totalAmount),
         backgroundColor: categoryAmounts.map(item => categoryColors[item.category]),
+        // borderColor: '#fff',
+        // borderWidth: 1,
       }],
     };
   };
 
   const months = [...new Set(data.map(item => item.month))];
   const chartData = useMemo(() => getPieChartData(selectedMonth), [data, selectedMonth]);
+  const totalExpense = chartData.datasets[0].data.reduce((sum, value) => sum + value, 0).toLocaleString('ko-KR');
+
   const options = {
+    responsive: true,
+    plugins: {
+      // 범례
+      legend: {
+        // display: false,
+        display: true,
+      },
+      tooltip: {
+        callbacks: {
+          label: function(context) {
+            const label = context.label || '';
+            const value = context.raw || 0;
+            const percentage = ((value / context.chart.data.datasets[0].data.reduce((sum, val) => sum + val, 0)) * 100).toFixed(2);
+            return `${label}: ${value.toLocaleString('ko-KR')}원 (${percentage}%)`;
+          }
+        }
+      },
+      datalabels: {
+        display: true,
+        color: 'black',
+        font: {
+          family: 'GowunDodum-Regular', // 글씨체 설정
+          size: 13, // 글씨 크기 설정
+          // weight: 'bold', // 글씨 굵기 설정
+        },
+        formatter: (value, context) => {
+          const percentage = ((value / context.chart.data.datasets[0].data.reduce((sum, val) => sum + val, 0)) * 100).toFixed(2);
+          if (context.dataIndex < 10 && percentage > 5) {
+          // if (context.dataIndex < 10 && ) {
+            // const percentage = ((value / context.chart.data.datasets[0].data.reduce((sum, val) => sum + val, 0)) * 100).toFixed(2);
+            return `${context.chart.data.labels[context.dataIndex]}:${value.toLocaleString('ko-KR')}원\n(${percentage}%)`;
+          }
+          return null;
+        },
+        anchor: 'end',
+        align: 'start',
+        offset: 10,
+      }
+    },
     elements: {
       arc: {
-        borderColor: 'rgba(0,0,0,0)', // 테두리 색상을 투명하게 설정
-      },
-    },
+        // borderColor: 'rgba(0,0,0,0)', // 테두리 색상을 투명하게 설정
+        borderWidth: 1
+
+      }
+    }
   };
-
-
 
   return (
     <div>
-      <h1>Monthly Expense Pie Chart </h1>
-      {/* <div className='current-month'>{selectedMonth}월</div> */}
+      <h1 style={{ textAlign: 'center' }}>
+        Monthly Expense Pie Chart
+      </h1>
       {error && <div className="error-message">{error}</div>}
       {data.length > 0 ? (
         <div>
@@ -79,6 +125,9 @@ const PieChartPage = () => {
           </div>
           <div className="chart-container">
             <Pie data={chartData} options={options}/>
+          </div>
+          <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <h2 >Total Expense: {totalExpense}원</h2>
           </div>
         </div>
       ) : (
