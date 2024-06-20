461b8929b2636f193374aa7f1ca6a852eca7f80a
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-06-20 20:59:44 +0900

    style: <NavLink>로 네비게이션 구현, /bar 링크 변경

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,48 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import BarChartPage from './PageBarChart';
 import PieChartPage from './PagePieChart';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Bar Chart</Link>
-            </li>
-            <li>
-              <Link to="/pie">Pie Chart</Link>
-            </li>
-          </ul>
+      <div style={{ textAlign: 'center' }}>
+        <p></p>
+        <h1>Monthly Expense Tracker</h1>
+        <nav style={{ marginBottom: '20px' }}>
+          <NavLink to="/bar" exact style={buttonStyle} activeStyle={activeButtonStyle}>
+            Bar Chart
+          </NavLink>
+          <NavLink to="/pie" style={buttonStyle} activeStyle={activeButtonStyle}>
+            Pie Chart
+          </NavLink>
         </nav>
 
         <Routes>
-          <Route path="/" element={<BarChartPage />} />
+          <Route path="/bar" element={<BarChartPage />} />
           <Route path="/pie" element={<PieChartPage />} />
         </Routes>
       </div>
     </Router>
   );
+};
+
+const buttonStyle = {
+  display: 'inline-block',
+  padding: '10px 20px',
+  backgroundColor: '#94b1d0',
+  color: '#fff',
+  textDecoration: 'none',
+  borderRadius: '5px',
+  margin: '0 10px',
+  cursor: 'pointer',
+  fontWeight: 'bold',
+};
+
+const activeButtonStyle = {
+  ...buttonStyle,
+  backgroundColor: '#568fcb',
 };
 
 export default App;
